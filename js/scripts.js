b718c844532941e4dc07532c08bd046acb32bbc2
jacob ruleaux <jakeruleaux>
2017-06-16 09:51:21 -0700

    add input fileds to js

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,22 @@
 
 $(document).ready(function() {
 
+
+$("form#new-order").submit(function(event) {
+  event.preventDefault();
+  var toppings = [];
+  $("input:checkbox[name=size]:checked").each(function() {
+    var size = $(this).val();
+    toppings.push(size);
+  });
+  $("input:checkbox[name=meat]:checked").each(function() {
+    var meatTop = $(this).val();
+    toppings.push(meatTop);
+  });
+  $("input:checkbox[name=veg]:checked").each(function() {
+    var vegTop = $(this).val();
+    toppings.push(vegTop);
+  });
+});
+
 });
