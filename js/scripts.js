dcda9a05087b47ab691e89b805b9759f5182cf41
jacob ruleaux <jakeruleaux>
2017-06-16 14:04:40 -0700

    add prototype to display pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,48 @@ function Pizza(pieSize, meat, veg) {
 }
 
 
-
-
+// var price = {
+//   meat: "2",
+//   veg: "1",
+//   calC: function() {
+//     return this.meat + this.veg;
+//   }
+// };
 
  var smallPie = {pieSize: "small", price: 16.00};
  var largePie = {pieSize: "large", price: 18.00};
  var vegCost = {veg: ["peppers", "olives", "onion", "tomato", "mushroom", "pineapple", "spinach", "fresh basil", "pepperoncini"], price: 1.00 };
  var meatCost = {meat: ["pepperoni", "sausage", "salami", "candian bacon", "ground beef", "chicken"], price: 2.00 };
 
-
-
-
-Pizza.prototype.pizzaCost = function() {
-   this.price = meatCost + "1" ;
-   return this.price;
+var vegTopCost = "";
+ for (var i = 0; i < vegCost.length; i += 1) {
+   vegTopCost = vegCost[i];
+// Pizza.prototype.pizzaCost = function() {
+//   meat = "2";
+//   veg = "1";
+//   this.price = meat + veg;
+//   return this.price;
+// }
+Pizza.prototype.pieBig = function() {
+  if (this.pieSize === 16) {
+    return this.pieSize = "16";
+  } else {
+    return this.pieSize = "18";
+  }
 }
 
 
-console.log(Pizza);
+Pizza.prototype.pizzaCost = function() {
+   this.price = this.meat + "1" ;
+   return this.price;
 
+   console.log(price);
+   console.log(this.price);
 
+}
 
+}
+console.log()
 
 ////////////////////////DO NOT CROSS////////////////////////////
 
@@ -40,13 +61,13 @@ $("form#new-order").submit(function(event) {
     var meat = [];
     var veg = [];
     var newPizza = new Pizza(pieSize, meat, veg);
+    console.log(Pizza);
 
-    console.log(meat);
   $("input:checkbox[name=size]:checked").each(function() {
     var size = $(this).val();
     pieSize.push(size);
     console.log(size);
-  // });
+  });
   $("input:checkbox[name=meat]:checked").each(function() {
     var meatTop = $(this).val();
     meat.push(meatTop);
@@ -62,11 +83,11 @@ $("form#new-order").submit(function(event) {
   });
 
   console.log(meat);
-  console.log(Pizza);
+  console.log(meat);
 
-
+var diameter = (newPizza.pieBig);
+$("#total").text(pieSize);
 var price = (newPizza.pizzaCost);
-$("ul#total").text(price);
-});
+$("#total").text(price);
 });
 });
