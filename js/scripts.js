c1079cc9d525272920b8cc1225bf28acfb9c7e8c
jacob ruleaux <jakeruleaux>
2017-06-16 13:16:45 -0700

    add protoytpe and user output function

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,28 @@ function Pizza(pieSize, meat, veg) {
   this.pieSize = 0;
   this.meat = 0;
   this.veg = 0;
-  // this.price = price;
+  this.price = 0;
 }
 
 
 
 
 
-// var smallPie = {pieSize: "small", price: 16.00};
-// var largePie = {pieSize: "large", price: 18.00};
-// var vegCost = {veg: ["peppers", "olives", "onion", "tomato", "mushroom", "pineapple", "spinach", "fresh basil", "pepperoncini"], price: 1.00 };
-// var meatCost = {meat: ["pepperoni", "sausage", "salami", "candian bacon", "ground beef", "chicken"], price: 2.00 };
-// var cost = (veg + meat + smallPie + largePie)
+ var smallPie = {pieSize: "small", price: 16.00};
+ var largePie = {pieSize: "large", price: 18.00};
+ var vegCost = {veg: ["peppers", "olives", "onion", "tomato", "mushroom", "pineapple", "spinach", "fresh basil", "pepperoncini"], price: 1.00 };
+ var meatCost = {meat: ["pepperoni", "sausage", "salami", "candian bacon", "ground beef", "chicken"], price: 2.00 };
+
+
+
+
 Pizza.prototype.pizzaCost = function() {
-  return this.price = this.pieSize + this.meat + this.veg;
+   this.price = meatCost + "1" ;
+   return this.price;
 }
-console.log();
+
+
+console.log(Pizza);
 
 
 
@@ -29,30 +35,38 @@ $(document).ready(function() {
 
 $("form#new-order").submit(function(event) {
   event.preventDefault();
-  var pieSize = [];
-    console.log(pieSize);
+    // console.log(pieSize);
+    var pieSize = [];
+    var meat = [];
+    var veg = [];
+    var newPizza = new Pizza(pieSize, meat, veg);
+
+    console.log(meat);
   $("input:checkbox[name=size]:checked").each(function() {
     var size = $(this).val();
     pieSize.push(size);
-    console.log(pieSize);
+    console.log(size);
   // });
-  var meat = [];
   $("input:checkbox[name=meat]:checked").each(function() {
     var meatTop = $(this).val();
     meat.push(meatTop);
+    console.log();
   // });
-  var veg = [];
   $("input:checkbox[name=veg]:checked").each(function() {
     var vegTop = $(this).val();
     veg.push(vegTop);
-    console.log(veg);
-    var newPizza = new Pizza(meat, veg, pieSize);
+    console.log();
   });
+
+    console.log(veg);
+  });
+
   console.log(meat);
   console.log(Pizza);
 
-$("ul#total").text("");
-});
+
+var price = (newPizza.pizzaCost);
+$("ul#total").text(price);
 });
 });
 });
