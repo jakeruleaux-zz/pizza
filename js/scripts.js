3947bd56204c37dae16d1dae990393618b8809c7
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-20 07:45:43 -0700

    build veg prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ function Pizza (pieSize, meat, veg) {
   this.veg = 0;
   this.price = 0;
 };
+Pizza.prototype.vegCost = function() {
+  if (this.veg === 2) {
+    return this.veg = "2";
+  }
+};
 
 Pizza.prototype.pieBig = function() {
   if (this.pieSize === 16) {
@@ -11,23 +16,23 @@ Pizza.prototype.pieBig = function() {
   } else {
     return this.pieSize = "18";
   }
-  console.log(pieBig);
 };
-Pizza.prototype.toppingCost = function() {
-  if (this.veg === 2) {
-    this.veg = (2 * this.veg.length);
-    return this.veg;
-  } else (this.meat === 3)
-    this.meat = (3 * this.meat.length);
-    return this.meat;
-    console.log(this.meat);
-  };
+
+// Pizza.prototype.toppingCost = function() {
+//   if (this.veg === 2) {
+//     this.veg = (2 * this.veg.length);
+//     return this.veg;
+//   } else (this.meat === 3)
+//     this.meat = (3 * this.meat.length);
+//     return this.meat;
+//     console.log(this.meat);
+//   };
 
 
-Pizza.prototype.pizzaCost = function() {
-  this.price = (this.meat + this.veg + this.pieSize) ;
-  return this.price;
-};
+// Pizza.prototype.pizzaCost = function() {
+//   this.price = (this.meat + this.veg + this.pieSize) ;
+//   return this.price;
+// };
 // debugger
 // console.log()
 
@@ -35,17 +40,13 @@ Pizza.prototype.pizzaCost = function() {
 
 $(document).ready(function() {
 
-  console.log(Pizza);
   var pieSize = [];
   var meat = [];
   var veg = [];
   //  var newPizza = new Pizza(pieSize,veg, meat);
-  console.log(veg);
-  console.log();
 
-  $("form#new-order").submit(function(event) {
+$("form#new-order").submit(function(event) {
     event.preventDefault();
-    // console.log(pieSize);
 
     var newPizza = new Pizza(pieSize,veg, meat);
     $("input:checkbox[name=size]:checked").each(function() {
@@ -58,7 +59,7 @@ $(document).ready(function() {
         meat.push(meatTop);
         console.log();
     });
-      $("input:checkbox[name=veg]:checked").each(function() {
+    $("input:checkbox[name=veg]:checked").each(function() {
         var vegTop = $(this).val();
         veg.push(vegTop);
         console.log(veg);
@@ -66,6 +67,8 @@ $(document).ready(function() {
 
 //    var vegTotal = (newPizza.vegCost);
 //    $("#total").text(veg);
+    var vegTotal = (newPizza.vegCost);
+    $("#total").text(veg);
     var diameter = (newPizza.pieBig);
     $("#total").text(pieSize);
   //  var price = (newPizza.pizzaCost);
